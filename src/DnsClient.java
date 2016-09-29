2598b6c9372cce11dfb883296de69a9b55e41a06
evan01 <eknoxmobile@gmail.com>
2016-09-29 15:46:35 -0400

    3/4 Done Parsere

--- src/DnsClient.java
+++ src/DnsClient.java
@@ -1,5 +1,13 @@
 public class DnsClient {
+	/**
+	 * The start of our program!
+	 * @param args
+	 */
 	public static void main (String args[]) {
 		
+	}
+
+	public static void createSocket(){
+
 	}
 }
--- src/Parser.java
+++ src/Parser.java
@@ -4,15 +4,21 @@
  */
 import org.apache.commons.cli.*;
 
+import java.util.concurrent.ExecutionException;
+import java.util.regex.*;
 import java.util.List;
 
 public class Parser {
 
     public static void main(String[] args){
-        parse(args);
+        //First get all the possible inputs into the program
+        Options options = getOptions();
 
+        //Next parse the options and get all the values the user entered
+        parse(args,options);
     }
-    public static void parse(String[] args){
+
+    public static Options getOptions(){
         Options options = new Options();
 
         Option timeoutOption = new Option("t", "timeout", true, "(Optional) Gives how long to wait, in seconds, before retransmitting an unanswered query");
@@ -35,26 +41,53 @@ public class Parser {
         serverOptionNS.setRequired(false);
         options.addOption(serverOptionNS);
 
-        Option ServerAddress = new Option("server","(Required) is the ipv4 address of the DNS server in an a.b.c.d format");
-        ServerAddress.setRequired(true);
-        options.addOption(ServerAddress);
+//        Option ServerAddress = new Option("server","(Required) is the ipv4 address of the DNS server in an a.b.c.d format");
+//        ServerAddress.setRequired(true);
+//        options.addOption(ServerAddress);
+//
+//        Option ServerName = new Option("name","(Required) is the domain name to query for");
+//        ServerName.setRequired(true);
+//        ServerName.setValueSeparator('@');
+//        options.addOption(ServerName);
 
-        Option Name = new Option("name","(Required) is the domain name to query for");
-        Name.setRequired(true);
-        options.addOption(Name);
+        return options;
+    }
 
+    public static String validateAndGetIP(String ip) throws ParseException{
+        //User Regex to make sure we are getting a valid ip address
+        String IPpattern = "@\\d{3}.\\d{3}.\\d{3}.\\d{3}";
+        //Create a pattern object
+        Pattern r = Pattern.compile(IPpattern);
+        // Now create matcher object.
+        Matcher m = r.matcher(ip);
+        if (m.find()){
+            return m.group(0).substring(1);
+        }else {
+            throw new ParseException("IP Address is invalid");
+        }
+    }
 
+    public static void parse(String[] args,Options options){
+        //Create some of the standard cli classes that help with parsing
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd;
 
+        //Start parsing the arguments given by the user
         try {
-            cmd = parser.parse(options, args);
-            if(cmd.hasOption("t")) {
-                // print the date and time
-                System.out.println("Has t!!");
-            }
-            System.out.println("Done");
+            cmd = parser.parse(options,args);
+
+            //First do the required arguments
+            String name = args[args.length-1];
+            String server = args[args.length-2];
+
+            //Validate that the server is in the right format, throws exception if not valid, also get rid of @character
+            server = validateAndGetIP(server);
+
+            //Next handle the options
+
+
+
 
         } catch (ParseException e) {
             System.out.println(e.getMessage());
@@ -63,11 +96,5 @@ public class Parser {
             System.exit(1);
             return;
         }
-
-        String inputFilePath = cmd.getOptionValue("input");
-        String outputFilePath = cmd.getOptionValue("output");
-
-        System.out.println(inputFilePath);
-        System.out.println(outputFilePath);
     }
 }
