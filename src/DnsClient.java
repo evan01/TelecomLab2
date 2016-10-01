470006fd31d0eacd6655d779de327fff4ee0e020
Chianyu Liu <Chianyu Liu>
2016-10-01 15:47:50 -0400

    add DnsClinent

--- src/DnsClient.java
+++ src/DnsClient.java
@@ -1,5 +1,10 @@
+
+import java.net.*;
+
 public class DnsClient {
-	public static void main (String args[]) {
+	public static void main (String args[]) {				
 		
-	}
+		
+		DatagramSocket socket = new DatagramSocket();
+	}	
 }
